6e8747aab51c24932c35355d1db9d8e951a2795a
alexa <alexander_corral_valdez@outlook.com>
2022-07-11 13:12:46 -0500

    uniat-Location

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View} from 'react-native';
+import * as Location from 'expo-location';
+import style from './style';
 
+const API_KEY = "";
+const URL = `https://maps.google.com/maps/api/geocode/json?key=${API_KEY}&latlng=`;
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [address, setAddress] =useState("loading...");
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    function setPosition({coords: {latitude, longitude}}) {
+      setLongitude(longitude);
+      setLatitude(latitude);
+      fetch(`${URL}${latitude}${longitude}`)
+        .then((resp) => resp.json())
+        .then(({results}) => {
+          if(results.length > 0) {
+            setAddress(results[0].formatted_address);
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+
+    let watcher;
+
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permiso denegado");
+        return;
+      }
+      
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition(location);
+
+      watcher = await Location.watchPositionAsync(
+        {accuracy: Location.LocationAccuracy.Highest}, 
+        setPosition
+      );
+    })();
+
+    return () => {
+      watcher?.remove();
+    };
+  },[]);
+
+  return(
+    <View style={style.container}>
+      <Text style={style.address}>Address: {address}</Text>
+      <Text style={style.label}>Latitude: {latitude}</Text>
+      <Text style={style.label}>Longitude: {longitude}</Text>
+    </View>
+  )
+}
